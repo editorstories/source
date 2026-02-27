30dc2548e5a3f136647bdf52d0f5733cb50ef158
project-infoprod <alias@protonmail.com>
2026-02-27 22:36:40 +0100

    Uppdateringar till språk och config.ts samt 'next': '16.1.5',

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
+// next.config.ts
 import type { NextConfig } from 'next'
-import path from 'path'
-
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/projekt' : '',
-  assetPrefix: isProd ? '/projekt/' : '',
-  trailingSlash: true,
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
+  // Inga basePath eller output-inställningar här
   reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
 }
 
 export default nextConfig
